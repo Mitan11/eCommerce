2605ffcd1effa8e92bc9c4194942e0b5f70a3b8f
mitan11 <mitantank00@gmail.com>
2025-04-10 18:56:31 +0530

    Enhance admin and user dashboards with new layout and functionality, including profile route and improved product/category displays.

---
+++ controllers/adminController.js
@@ -0,0 +1,11 @@
+const profile = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    profile
+}
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -7,6 +7,11 @@ const adminRouter = express.Router();
 
 adminRouter.get('/logout', logout)
 
+
+adminRouter.get('/profile', auth, async (req, res) => {
+    res.render('admin/profile', { user: req.user });
+})
+
 adminRouter.get('/dashboard', auth, (req, res) => {
     res.render('admin/dashboard', { user: req.user });
 })
